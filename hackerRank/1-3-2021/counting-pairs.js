cce5cfa2bbaf2234db40a870f8101679afcc2961
Emmett He <emmetthe26@gmail.com>
2022-01-14 10:46:01 -0800

    hacker rank

--- hackerRank/1-3-2021/counting-pairs.js
+++ hackerRank/1-3-2021/counting-pairs.js
@@ -7,6 +7,26 @@
  *  2. INTEGER k
  */
 
+// Example
+// n = 4
+// numbers = [1, 1, 1, 2]
+// k = 1
+
+// This array has three different valid pairs: (1, 1) and (1, 2) and (2, 2). For k = 1, there is only 1 valid pair which satisfies a + k = b: the pair (a, b) = (1, 2).
+
 function countPairs(numbers, k) {
   // Write your code here
+
+  let nums = new Set();
+  let pairs = new Set();
+
+  for (let i = 0; i < numbers.length; i++) nums.add(numbers[i]);
+
+  for (let num of nums) {
+    let complement = num + k;
+    let pair = [num, complement].sort((a, b) => a - b);
+    if (nums.has(complement)) pairs.add(pair);
+  }
+
+  return pairs.size;
 }
