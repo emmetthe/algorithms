0bd0ba79c69490d0b6fec262de65e397a9645b15
Emmett He <emmetthe26@gmail.com>
2022-02-24 17:32:05 -0800

    simplify

--- leetcode/2.js
+++ leetcode/2.js
@@ -1,19 +1,31 @@
 var addTwoNumbers = function (l1, l2) {
-  let carry = 0;
-  let head = new ListNode();
-  curr = head;
+  const ans = new ListNode(0); // sum will be stored in ans
+  let carry = 0,
+    curr = ans; // answer will get updated in curr
+
   while (l1 || l2) {
-    let x = l1 !== null ? l1.val : 0;
-    let y = l2 !== null ? l2.val : 0;
-    sum = x + y + carry;
-    carry = Math.floor(sum / 10);
-    curr.next = new ListNode(sum % 10);
-    if (l1 !== null) l1 = l1.next;
-    if (l2 !== null) l2 = l2.next;
+    // checks whether l1 or l2 is null, if not null then continues evaluation of loop
+
+    // val1  & val2 will contain the current value of node
+    const val1 = l1 ? l1.val : 0;
+    const val2 = l2 ? l2.val : 0;
+
+    // if the next node is not empty then next node value will be stored in l1
+    l1 = l1 ? l1.next : null;
+    l2 = l2 ? l2.next : null;
+
+    const sum = carry + val1 + val2;
+    const value = sum % 10;
+
+    carry = sum >= 10 ? 1 : 0; // since at most carry can be 1 and minimum carry will be 0
+
+    curr.next = new ListNode(value); // the current will contain the sum
     curr = curr.next;
   }
+
+  // if there is no next & we still have a carry then we have to add it using
   if (carry) {
-    curr.next = new ListNode(carry);
+    curr.next = new ListNode(1);
   }
-  return head.next;
+  return ans.next; //here we are returning ans.next because we have added a 0 in   ans at start.
 };
